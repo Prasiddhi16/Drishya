442a44b967b790a699623b45d8e3fd04e00d6b11
Prasiddhi16-63 <prasiddhi1624@student.ku.edu.np>
2025-07-05 16:59:28 +0545

    window changes

--- analysiswindow.cpp
+++ analysiswindow.cpp
@@ -14,37 +14,33 @@ analysisWindow::~analysisWindow()
 }
 
 void analysisWindow::on_pushButton_clicked()
-{   this->hide();
-    budget_window = new  budgetWindow(nullptr);
-    connect(budget_window, &budgetWindow::windowClosed,this, &analysisWindow::show);
+{
+    this->hide();
+    budget_window = new budgetWindow(nullptr);
+    connect(budget_window, &budgetWindow::windowClosed, this, &analysisWindow::show);
     budget_window->show();
-
 }
 
-
 void analysisWindow::on_pushButton_2_clicked()
-{   this->hide();
-    rev_window= new revWindow(nullptr);
-    connect(rev_window, &revWindow::windowClosed,this, &analysisWindow::show);
+{
+    this->hide();
+    rev_window = new revWindow(nullptr);
+    connect(rev_window, &revWindow::windowClosed, this, &analysisWindow::show);
     rev_window->show();
 }
 
-
 void analysisWindow::on_pushButton_3_clicked()
-{   this->hide();
-    weekly_window= new weeklyWindow(nullptr);
-    connect(weekly_window, &weeklyWindow::windowClosed,this, &analysisWindow::show);
+{
+    this->hide();
+    weekly_window = new weeklyWindow(nullptr);
+    connect(weekly_window, &weeklyWindow::windowClosed, this, &analysisWindow::show);
     weekly_window->show();
-
 }
-
-
 
 void analysisWindow::on_pushButton_4_clicked()
 {
     this->hide();
-    monthly_window= new monthlyWindow(this);
-    connect(monthly_window, &monthlyWindow::windowClosed,this, &analysisWindow::show);
+    monthly_window = new monthlyWindow(nullptr);
+    connect(monthly_window, &monthlyWindow::windowClosed, this, &analysisWindow::show);
     monthly_window->show();
 }
-
