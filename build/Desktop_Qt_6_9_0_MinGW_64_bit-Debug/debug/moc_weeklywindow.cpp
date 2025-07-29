771b7d0f4ed8966880604ce0c9d0b231b6dd5940
Prasiddhi16-63 <prasiddhi1624@student.ku.edu.np>
2025-07-29 20:26:32 +0545

    version1

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/debug/moc_weeklywindow.cpp
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/debug/moc_weeklywindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../checkkk/weeklywindow.h"
+#include "../../../weeklywindow.h"
 #include <QtCore/qmetatype.h>
 
 #include <QtCore/qtmochelpers.h>
